82760b607fde566b6abeeb8835452cf606176a51
HilaryOkello <hilaryokello091@gmail.com>
2025-07-17 12:27:33 +0300

    feat; select longitude and latitude on map click

--- src/components/map/DynamicMap.tsx
+++ src/components/map/DynamicMap.tsx
@@ -1,7 +1,17 @@
 import dynamic from 'next/dynamic';
+import { LatLng } from 'leaflet';
+
+interface MapProps {
+  onMapClick: (latLng: LatLng) => void;
+  selectedPosition: LatLng | null;
+  center: [number, number];
+  zoom: number;
+}
 
 const DynamicMap = dynamic(() => import('./Map'), {
   ssr: false,
-});
+}) as React.ComponentType<MapProps>;
 
 export default DynamicMap;
+
+
